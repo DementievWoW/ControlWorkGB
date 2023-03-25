9d305af368703e3ab537faff5ee6eba9a64635e4
DementievWoW <dementievwow@mail.ru>
2023-03-25 23:34:26 +0300

    Добавление папки resourses с блоксхемой проекта

--- ControlWorkGB/Program.cs
+++ ControlWorkGB/Program.cs
@@ -1,43 +1,43 @@
 ﻿Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt16(Console.ReadLine());
-string[] array=new string[size];
+string[] initalyArray=new string[size];
 Console.ForegroundColor = ConsoleColor.DarkGreen;
 Console.WriteLine("Внимание! Заполнение массива происходит с 1 элемента");
 Console.ForegroundColor = ConsoleColor.White;
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < initalyArray.Length; i++)
 {
     Console.WriteLine($"Введите {i+1} элемент из {size}");
-    array[i] = Console.ReadLine() ?? ""; // если null, то "" строка
+    initalyArray[i] = Console.ReadLine() ?? ""; // если null, то "" строка
 }
 size= 0;
-foreach (var a in array)
+foreach (var a in initalyArray)
 {
     if (a.Length<=3)
     {
         size++;
     }
 }
-string[] array2 = new string[size];
+string[] processedArray = new string[size];
 int tmp = 0;
-foreach (var a in array)
+foreach (var a in initalyArray)
 {
     if (a.Length <= 3)
     {
-        array2[tmp] = a;
+        processedArray[tmp] = a;
         tmp++;
     }
 }
 
 Console.WriteLine("Изначальный массив");
 Console.Write("[");
-foreach (var item in array)
+foreach (var item in initalyArray)
 {
     Console.Write(" "+item+", ");
 }
 Console.WriteLine("]");
 Console.WriteLine("Конечный массив");
 Console.Write("[");
-foreach (var item in array2)
+foreach (var item in processedArray)
 {
     Console.Write(" " + item + ", ");
 }
