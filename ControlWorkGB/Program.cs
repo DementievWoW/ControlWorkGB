031cef4e15fb86a720e6224af545dbe56a528e05
DementievWoW <dementievwow@mail.ru>
2023-03-25 22:33:42 +0300

    Заполнение массива

--- ControlWorkGB/Program.cs
+++ ControlWorkGB/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt16(Console.ReadLine());
+string[] array=new string[size];
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Внимание! Заполнение массива происходит с 1 элемента");
+Console.ForegroundColor = ConsoleColor.White;
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите {i+1} элемент из {size}");
+    array[i] = Console.ReadLine() ?? "";
+}
