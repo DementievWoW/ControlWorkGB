01de613989ac3834d1083f091e92535faa048fb9
DementievWoW <dementievwow@mail.ru>
2023-03-25 22:47:04 +0300

    Ready

--- ControlWorkGB/Program.cs
+++ ControlWorkGB/Program.cs
@@ -7,5 +7,38 @@ Console.ForegroundColor = ConsoleColor.White;
 for (int i = 0; i < array.Length; i++)
 {
     Console.WriteLine($"Введите {i+1} элемент из {size}");
-    array[i] = Console.ReadLine() ?? "";
+    array[i] = Console.ReadLine() ?? ""; // если null, то "" строка
 }
+size= 0;
+foreach (var a in array)
+{
+    if (a.Length>=3)
+    {
+        size++;
+    }
+}
+string[] array2 = new string[size];
+int tmp = 0;
+foreach (var a in array)
+{
+    if (a.Length >= 3)
+    {
+        array2[tmp] = a;
+        tmp++;
+    }
+}
+
+Console.WriteLine("Изначальный массив");
+Console.Write("[");
+foreach (var item in array)
+{
+    Console.Write(" "+item+", ");
+}
+Console.WriteLine("]");
+Console.WriteLine("Конечный массив");
+Console.Write("[");
+foreach (var item in array2)
+{
+    Console.Write(" " + item + ", ");
+}
+Console.WriteLine("]");
